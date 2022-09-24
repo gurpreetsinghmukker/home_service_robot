4a71f4b6b2e569247c9d34efe80afb5ecac18b14
Gurpreet <gurpreetsinghmukker@gmail.com>
2022-09-23 22:38:30 -0400

    added add_markers shell file

---
+++ catkin_ws/src/scripts/add_markers.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+xterm -e " pwd; source /opt/ros/kinetic/setup.bash; roscore" &
+sleep 5
+xterm -e " cd ../../; source devel/setup.bash; roslaunch my_robot world.launch " &
+#sleep 5
+#xterm -e " cd ../../; source devel/setup.bash; roslaunch gmapping slam_gmapping_pr2.launch " &
+#sleep 5
+#xterm -e " cd ../../; source devel/setup.bash; roslaunch my_robot teleop.launch " &
+sleep 5
+xterm -hold -e " cd ../../; source devel/setup.bash; roslaunch my_robot amcl.launch " &
+sleep 5
+xterm -hold -e " cd ../../; source devel/setup.bash; roslaunch my_robot add_markers.launch " &
+#sleep 5
+#xterm -hold -e " cd ../../; source devel/setup.bash; roslaunch my_robot pick_objects.launch " &
+sleep 5
+xterm -e " rosrun rviz rviz"
